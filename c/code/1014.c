adb51c2f293f9e5b10fdf0a83dfa7f3bd8565c3c
HaoQing <haoqing@HaoQingdeMacBook-Air.local>
2015-12-20 16:36:11 +0800

    1014 OK

--- c/code/1014.c
+++ c/code/1014.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define MAX 26
 typedef enum {
     false, 
     true
@@ -8,61 +9,64 @@ typedef enum {
 
 typedef struct trieNode* TrieNode;
 struct trieNode {
-    TrieNode    trieNode;   // child node
-    int         trieNum;    // child node number.
-    int         prefixNum;  // words number that begin with this value.
-    char        value;      // value of the node.
-    bool        isWord;     // contains current value is a word.
+    TrieNode    trieNode[MAX];   // child node
+    int         len;             // words number that begin with this value.
+    bool        isWord;          // contains current value is a word.
 };
 
 TrieNode initTrieNode() {
-    TrieNode trieNodeNew    = (TrieNode) malloc(sizeof(struct trieNode));
-    trieNodeNew->trieNode   = (TrieNode) malloc(26*sizeof(struct trieNode));
-    trieNodeNew->trieNum    = 0;
-    trieNodeNew->prefixNum  = 0;
-    trieNodeNew->value      = 0x0;
-    trieNodeNew->isWord     = false;
+    int i = 0;
+    TrieNode trieNodeNew  = (TrieNode) malloc(sizeof(struct trieNode));
+    trieNodeNew->len      = 0;
+    trieNodeNew->isWord   = false;
+    for(i = 0; i < MAX; i++) {
+        trieNodeNew->trieNode[i] = NULL;
+    }
     return trieNodeNew;
 }
-void buildTrieTree(char* nextValues, TrieNode parentNode) {
-    bool needAddNew = true;
-    int i = 0;
-    if(nextValues == NULL || (strncmp(nextValues, "", 1) == 0 )) {
-        if(parentNode == NULL) {
-            return;
-        } else {
-           parentNode->isWord = true;
-           return;
-        }
-    }
 
-    int num = parentNode->trieNum;
-    for(i = 0; i < num; i++) {
-        if(parentNode->trieNode[num].value == nextValues[0]) {
-            needAddNew = false;
-            parentNode->trieNode[i].prefixNum++;
-            buildTrieTree(&nextValues[1], &parentNode->trieNode[i]);
-        }
+void buildTrieTree(const char* input, TrieNode root) {
+    if (root == NULL || *input == '\0') {
+        return;
     }
+    TrieNode p  = root;
 
-    if (needAddNew) {
-        parentNode->trieNum++;
-        parentNode->trieNode[num].prefixNum = 1;
-        parentNode->trieNode[num].value = nextValues[0];
-        parentNode->trieNode[num].trieNode = (TrieNode) malloc(26*sizeof(struct trieNode));
-        buildTrieTree(&nextValues[1], &parentNode->trieNode[num]);
+    while(*input != '\0') {
+        int index = *input - 'a';
+        if(p->trieNode[index] == NULL) {
+            TrieNode trieNode = initTrieNode();
+            p->trieNode[index] = trieNode; //soga!!!
+        }
+        p = p->trieNode[index];
+        p->len++;
+        input++;
     }
+    p->isWord = true;
     return ;
 }
 
-//void findPrefixNumInTrieTree(char* input, TrieNode* trieTree) {
-//}
+int findPrefixNumInTrieTree(const char* input, TrieNode root) {
+    if (root == NULL || *input == '\0') {
+        return 0;
+    }
+    TrieNode p  = root;
+
+    while(p != NULL && *input != '\0') {
+        int index = *input - 'a';
+        if(p->trieNode[index] == NULL)
+            return 0;
+        p = p->trieNode[index];
+        input++;
+    }
+    return p->len;
+}
 
 int main() {
-    int num = 0;
-    char *input = NULL;
-    TrieNode root = NULL;
+    int num     = 0;
+    int len     = 0; 
+    char input[100]; // should set inout[100] here.
 
+    TrieNode root = NULL;
     root = initTrieNode(); 
 
     scanf("%d", &num);
@@ -72,17 +76,13 @@ int main() {
         num--;
     }
 
-    scanf("%s", input);
-/*    char *input1 = "fs";
-    char *input2 = "fsd";
-    TrieNode root = NULL;
-    root = initTrieNode(); 
-    buildTrieTree(input1, root);
-    buildTrieTree(input2, root);
-*/
-    //    findPrefixNumInTrieTree(input, trieTree);
-
+    scanf("%d", &num);
+    while(num > 0) {
+        scanf("%s", input);
+        len = findPrefixNumInTrieTree(input, root);
+        printf("%d\n", len);
+        num--;
+    }
+    printf("\n");
     return 0;
 }
-
-
